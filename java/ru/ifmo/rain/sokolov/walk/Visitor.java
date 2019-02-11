b1d88c0b0da6551a07875c0a73a88448270f4460
damm1t <donatsokolov99@gmail.com>
2019-02-11 05:08:32 +0300

    add Visitor

---
+++ java/ru/ifmo/rain/sokolov/walk/Visitor.java
@@ -0,0 +1,44 @@
+package ru.ifmo.rain.sokolov.walk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class Visitor extends SimpleFileVisitor<Path> {
+    private PrintWriter writer;
+    private final String format = "%08x %s" + System.lineSeparator();
+
+    public Visitor(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    private FileVisitResult write(int hash, String filePath) throws IOException {
+        //HashCounter.getFNV1Hash(file)
+        writer.printf(format, hash, filePath);
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        writer.printf(format, HashCounter.getFNV1Hash(file), file.toString());
+        if (writer.checkError()) {
+            System.out.println("Output failed");
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        writer.printf(format, 0, file.toString());
+        if (writer.checkError()) {
+            System.out.println("Output failed");
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+}
