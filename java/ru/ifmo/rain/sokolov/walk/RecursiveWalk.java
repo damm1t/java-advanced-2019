f78b51d4d746492060051a109be324162cb2fe95
damm1t <donatsokolov99@gmail.com>
2019-02-11 05:08:52 +0300

    fix RecursiveWalk

--- java/ru/ifmo/rain/sokolov/walk/RecursiveWalk.java
+++ java/ru/ifmo/rain/sokolov/walk/RecursiveWalk.java
@@ -3,8 +3,10 @@ package ru.ifmo.rain.sokolov.walk;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UncheckedIOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RecursiveWalk {
     private final Path inputPath;
@@ -19,43 +21,35 @@ public class RecursiveWalk {
         }
     }
 
-    public void doWork() throws WalkException {
+    private static void walker(String path, PrintWriter writer) throws IOException {
+        try {
+            Path filePath = Paths.get(path);
+            try {
+                Files.walkFileTree(filePath, new Visitor(writer));
+            } catch (IOException e) {
+                writer.write(String.format("%08x", 0) + " " + filePath);
+            }
+        } catch (InvalidPathException e) {
+            writer.write(String.format("%08x", 0) + " " + path);
+        }
+    }
 
-        try (var input = Files.newBufferedReader(inputPath);
-             var output = new PrintWriter(Files.newBufferedWriter(outputPath))) {
-            final int[] line = {0};
-            final String format = "%08x %s" + System.lineSeparator();
+    private void doWork() throws WalkException {
 
-            input.lines().forEach(curPath -> {
-                line[0]++;
-                try {
-                    Files.walkFileTree(Paths.get(curPath), new SimpleFileVisitor<Path>() {
-                        @Override
-                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                            output.printf(format, HashCounter.getFNV1Hash(file), file.toString());
-                            if (output.checkError()) {
-                                System.out.println("Output failed");
-                            }
-                            return FileVisitResult.CONTINUE;
-                        }
-
-                        @Override
-                        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                            System.err.println("sooooqa");
-                            output.printf(format, 0, file.toString());
-                            if (output.checkError()) {
-                                System.out.println("Output failed");
-                            }
-                            return FileVisitResult.CONTINUE;
-                        }
-                    });
-                } catch (InvalidPathException e) {
-                    throw new WalkException("Invalid path (at file " + curPath + " on line " + line[0] + ")");
-
-                } catch (IOException | SecurityException e) {
-                    throw new WalkException("Failed to read file from \" + curPath + \" at line \" + line[0]");
+        try (var bufferedReader = Files.newBufferedReader(inputPath);
+             var writer = new PrintWriter(Files.newBufferedWriter(outputPath))) {
+            try {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    try {
+                        walker(line, writer);
+                    } catch (IOException e) {
+                        throw new WalkException("Failed to write to file from \\" + outputPath);
+                    }
                 }
-            });
+            } catch (IOException e) {
+                throw new WalkException("Failed to read file from \\" + inputPath);
+            }
         } catch (UncheckedIOException | IOException | SecurityException e) {
             throw new WalkException("Failed to open input/output files" + e.getMessage() + ")");
         }
@@ -64,7 +58,7 @@ public class RecursiveWalk {
     public static void main(String[] args) {
         try {
             if (args == null || args.length != 2 || args[0] == null || args[1] == null) {
-                throw new WalkException("invalid arguments\nExpected arguments: <input file> <output file>");
+                throw new WalkException("Invalid arguments\nExpected arguments: <input file> <output file>");
             }
             var dude = new RecursiveWalk(args[0], args[1]);
             dude.doWork();
