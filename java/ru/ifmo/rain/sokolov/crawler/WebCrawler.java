8b730b04a6f918ddd158b7f43a803f0194b5fdda
damm1t <donatsokolov99@gmail.com>
2019-04-30 01:49:54 +0300

    upd Crawler

--- java/ru/ifmo/rain/sokolov/crawler/WebCrawler.java
+++ java/ru/ifmo/rain/sokolov/crawler/WebCrawler.java
@@ -20,6 +20,17 @@ public class WebCrawler implements Crawler {
     private final int perHost;
     private final Map<String, TaskPoolPerHost> mapPerHost = new ConcurrentHashMap<>();
 
+    @Override
+    public Result download(String url, int depth) {
+        final Set<String> downloaded = ConcurrentHashMap.newKeySet();
+        final Map<String, IOException> errors = new ConcurrentHashMap<>();
+        var phaser = new Phaser(1);
+        downloadImplDfs(url, depth, downloaded, errors, phaser, site -> true);
+        phaser.arriveAndAwaitAdvance();
+        downloaded.removeAll(errors.keySet());
+        return new Result(new ArrayList<>(downloaded), new HashMap<>(errors));
+    }
+
     public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
         this.downloader = downloader;
         this.downloadersPool = Executors.newFixedThreadPool(downloaders);
@@ -27,78 +38,69 @@ public class WebCrawler implements Crawler {
         this.perHost = perHost;
     }
 
-    private TaskPoolPerHost getTaskPool(String hostname) {
-        synchronized (mapPerHost) {
-            mapPerHost.putIfAbsent(hostname, new TaskPoolPerHost());
-            return mapPerHost.get(hostname);
-        }
-    }
+    private void downloadImplDfs(String url, int depth, final Set<String> downloaded, final Map<String, IOException> errors,
+                                 Phaser phaser, Predicate<String> filter) {
 
-    private Result download(String url, int depth, Predicate<String> filter) {
-        var result = new ResultWrapper();
-        var barrier = new Phaser(1);
-        downloadImplDfs(url, depth, result, barrier, filter);
-        barrier.arriveAndAwaitAdvance();
-        return result.toResult();
-    }
-
-    @Override
-    public Result download(String url, int depth) {
-        return download(url, depth, site -> true);
-    }
-
-    private void downloadImplDfs(String url, int depth, ResultWrapper wrapper,
-                                 Phaser barrier, Predicate<String> filter) {
-        if (depth <= 0 || !filter.test(url) || !wrapper.downloaded.add(url)) {
+        if (depth <= 0 || !filter.test(url) || !downloaded.add(url)) {
             return;
         }
 
-        submitDownloader(url, wrapper, barrier, () -> {
-            try {
-                var document = downloader.download(url);
-                barrier.register();
-                extractorsPool.submit(() -> {
-                    try {
-                        document.extractLinks()
-                                .forEach(link ->
-                                        downloadImplDfs(link, depth - 1, wrapper, barrier, filter));
-                    } catch (IOException e) {
-                        wrapper.errors.put(url, e);
-                    } finally {
-                        barrier.arrive();
-                    }
-                });
-            } catch (IOException e) {
-                wrapper.errors.put(url, e);
-            }
-        });
-    }
-
-    private void submitDownloader(String url, ResultWrapper wrapper,
-                                  Phaser barrier, Runnable task) {
         try {
-            var host = URLUtils.getHost(url);
-            var taskPoolPerHost = getTaskPool(host);
-            taskPoolPerHost.submitDownloaderImpl(barrier, task, this);
+            final String host = URLUtils.getHost(url);
+            final TaskPoolPerHost data = mapPerHost.computeIfAbsent(host, s -> new TaskPoolPerHost());
+
+            phaser.register();
+
+            data.addTask(() -> {
+                try {
+                    var document = downloader.download(url);
+                    downloaded.add(url);
+                    phaser.register();
+                    extractorsPool.submit(() -> {
+                        try {
+                            document.extractLinks()
+                                    .forEach(link ->
+                                            downloadImplDfs(link, depth - 1, downloaded, errors, phaser, filter));
+                        } catch (IOException e) {
+                            errors.put(url, e);
+                        } finally {
+                            phaser.arrive();
+                        }
+                    });
+
+                } catch (IOException e) {
+                    errors.put(url, e);
+                } finally {
+                    phaser.arrive();
+                    data.nextTask();
+                }
+            });
         } catch (MalformedURLException e) {
-            wrapper.errors.put(url, e);
+            errors.put(url, e);
         }
     }
 
-    private Runnable transformTask(TaskPoolPerHost taskPool, Runnable task, Phaser barrier) {
-        return () -> {
-            task.run();
-            synchronized (taskPool.queueTasks) {
-                if (!taskPool.queueTasks.isEmpty()) {
-                    downloadersPool.submit(
-                            transformTask(taskPool, taskPool.queueTasks.poll(), barrier)
-                    );
-                } else {
-                    taskPool.threadCount--;
-                }
+    private class TaskPoolPerHost {
+        final Queue<Runnable> queueTasks = new ArrayDeque<>();
+        int threadCount = 0;
+
+        private synchronized void addTask(Runnable task) {
+            if (threadCount < perHost) {
+                ++threadCount;
+                downloadersPool.submit(task);
+            } else {
+                queueTasks.add(task);
             }
-            barrier.arrive();
-        };
+        }
+
+        private synchronized void nextTask() {
+            Runnable other = queueTasks.poll();
+            if (other != null) {
+                downloadersPool.submit(other);
+            } else {
+                --threadCount;
+            }
+        }
     }
 
     @Override
@@ -113,33 +115,4 @@ public class WebCrawler implements Crawler {
         }
     }
 
-    private static class ResultWrapper {
-        final Set<String> downloaded = ConcurrentHashMap.newKeySet();
-        final Map<String, IOException> errors = new ConcurrentHashMap<>();
-
-        Result toResult() {
-            downloaded.removeAll(errors.keySet());
-            return new Result(new ArrayList<>(downloaded), new HashMap<>(errors));
-        }
-    }
-
-    private static class TaskPoolPerHost {
-        final Queue<Runnable> queueTasks = new ArrayDeque<>();
-        int threadCount = 0;
-
-        private void submitDownloaderImpl(Phaser barrier, Runnable task, WebCrawler crawler) {
-            barrier.register();
-            synchronized (queueTasks) {
-                if (threadCount < crawler.perHost) {
-                    threadCount++;
-                    crawler.downloadersPool
-                            .submit(crawler
-                                    .transformTask(this, task, barrier));
-
-                } else {
-                    queueTasks.add(task);
-                }
-            }
-        }
-    }
 }
